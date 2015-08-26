113210084fc386bad95159af4f335e93333e7d15
dnawym <dnawym@163.com>
2015-08-26 18:12:33 +0800

    add comments for main.cpp

--- 4.1/main.cpp
+++ 4.1/main.cpp
@@ -47,7 +47,7 @@ bool is_last_chunk(data_chunk& data)
 }
 
 std::mutex mut;
-std::queue<data_chunk> data_queue;
+std::queue<data_chunk> data_queue;	// 用于线程间通信的队列 
 std::condition_variable data_cond;
 
 void data_preparation_thread()
@@ -57,8 +57,10 @@ void data_preparation_thread()
     	std::cout << "data_preparation_thread while" << std::endl;
         data_chunk const data=prepare_data();
         std::lock_guard<std::mutex> lk(mut);
+        // 数据准备好后，使用lock_guard来锁定信号量，将数据插入队列之中 
         data_queue.push(data);
         std::cout << "data_preparation_thread notify_one" << std::endl;
+        // 通过条件变量通知其它等待的线程 
         data_cond.notify_one();
     }
 }
@@ -68,12 +70,15 @@ void data_processing_thread()
     while(true)
     {
     	std::cout << "data_processing_thread while" << std::endl;
+    	// 使用unique_lock，因为我们需要在取得数据之后，处理数据之间，解锁mutex 
         std::unique_lock<std::mutex> lk(mut);
         std::cout << "data_processing_thread before wait" << std::endl;
+        // 等待条件满足，unique_lock和Lambda函数，判断数据队列是否为空 
         data_cond.wait(lk,[]{return !data_queue.empty();});
         std::cout << "data_processing_thread pass wait" << std::endl;
         data_chunk data=data_queue.front();
         data_queue.pop();
+        // 处理数据需要较多时间，所以先解锁mutex 
         lk.unlock();
         std::cout << "data_processing_thread process data" << std::endl;
         process(data);
